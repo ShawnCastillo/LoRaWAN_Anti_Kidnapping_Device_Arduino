ed1b9c691d0b08405b9d47f23e87ed7b79a1505e
ShawnCastillo <shawn_castillo@dlsu.edu.ph>
2023-10-02 22:24:04 +0800

    dev-shawn-website-4
    -added label for each location pin

--- homescreen.js
+++ homescreen.js
@@ -1,102 +1,105 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
+  document.getElementById("mySidenav").style.width = "250px";
+}
 
-  document.addEventListener('DOMContentLoaded', function() {
-    initMap();
-  });
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
 
-  const latitudeInput = document.getElementById('latitudeInput');
-  const longitudeInput = document.getElementById('longitudeInput');
-  const rssiInput = document.getElementById('rssiInput');
-  const csumErrorInput = document.getElementById('csumErrorInput');
+document.addEventListener('DOMContentLoaded', function() {
+  initMap();
+});
 
-  const allowNumbersOnly = (inputElement) => {
-      inputElement.addEventListener('input', function() {
-          this.value = this.value.replace(/[^0-9.]/g, ''); // Allow numbers and dots
-      });
-  };
+const latitudeInput = document.getElementById('latitudeInput');
+const longitudeInput = document.getElementById('longitudeInput');
+const rssiInput = document.getElementById('rssiInput');
+const csumErrorInput = document.getElementById('csumErrorInput');
 
-  allowNumbersOnly(latitudeInput);
-  allowNumbersOnly(longitudeInput);
-  allowNumbersOnly(rssiInput);
-  allowNumbersOnly(csumErrorInput);
-
-  const markers = [];
-  
-  function initMap() {
-    // Initial coordinates (default map center)
-    const initialCoordinates = { lat: 14.56516743474788, lng: 120.99321086784676 }; // Default is Henry Sy bldg. in DLSU
-  
-    // Create a new map centered at the specified coordinates
-    const map = new google.maps.Map(document.getElementById('map-container'), {
-      zoom: 18, // Adjust the initial zoom level as needed
-      center: initialCoordinates,
+const allowNumbersOnly = (inputElement) => {
+    inputElement.addEventListener('input', function() {
+        this.value = this.value.replace(/[^0-9.]/g, ''); // Allow numbers and dots
     });
-  
-    // Define a marker variable (we'll use it to display the device location)
-    let deviceMarker;
-  
-    // Array to store markers
+};
+
+allowNumbersOnly(latitudeInput);
+allowNumbersOnly(longitudeInput);
+allowNumbersOnly(rssiInput);
+allowNumbersOnly(csumErrorInput);
+
+const markers = [];
+
+function initMap() {
+  // Initial coordinates (default map center)
+  const initialCoordinates = { lat: 14.56516743474788, lng: 120.99321086784676 }; // Default is Henry Sy bldg. in DLSU
+
+  // Create a new map centered at the specified coordinates
+  const map = new google.maps.Map(document.getElementById('map-container'), {
+    zoom: 18, // Adjust the initial zoom level as needed
+    center: initialCoordinates,
+  });
+
+  // Define a marker variable (we'll use it to display the device location)
+  let deviceMarker;
+
+  // Array to store markers
 
 
-    // Function to set the marker at a specific location
-    function setDeviceLocation(latitude, longitude) {
-        // Create a marker at the specified coordinates
-        const marker = new google.maps.Marker({
-            position: { lat: parseFloat(latitude), lng: parseFloat(longitude) },
-            map: map,
-            title: 'Device Location',
-        });
+  // Function to set the marker at a specific location
+  function setDeviceLocation(latitude, longitude) {
+  // Create a formatted label text with 5 decimal points
+  const labelText = `Lat: ${parseFloat(latitude).toFixed(5)}, Lng: ${parseFloat(longitude).toFixed(5)}`;
 
-        // Center the map on the new marker's position and zoom in
-        map.setCenter(marker.getPosition());
-        map.setZoom(18); // Adjust the zoom level as needed
+  // Create a marker at the specified coordinates with the label
+  const marker = new google.maps.Marker({
+      position: { lat: parseFloat(latitude), lng: parseFloat(longitude) },
+      map: map,
+      title: labelText, // Set the label text
+  });
 
-        // Add the marker to the markers array
-        markers.push(marker);
+  // Center the map on the new marker's position and zoom in
+  map.setCenter(marker.getPosition());
+  map.setZoom(18); // Adjust the zoom level as needed
+
+  // Add the marker to the markers array
+  markers.push(marker);
+}
+
+  // Add an event listener to the "Locate My Device" button
+  document.getElementById('locateButton').addEventListener('click', function() {
+    // Get the latitude and longitude values from the input fields
+    const latitude = document.getElementById('latitudeInput').value;
+    const longitude = document.getElementById('longitudeInput').value;
+
+    // Check if both latitude and longitude are provided
+    if (latitude && longitude) {
+        // Set the device location on the map
+        setDeviceLocation(latitude, longitude);
+    } else {
+        // Show an alert if either latitude or longitude is missing
+        alert('Please enter both latitude and longitude.');
     }
-  
-    // Add an event listener to the "Locate My Device" button
-    document.getElementById('locateButton').addEventListener('click', function() {
-      // Get the latitude and longitude values from the input fields
-      const latitude = document.getElementById('latitudeInput').value;
-      const longitude = document.getElementById('longitudeInput').value;
-  
-      // Check if both latitude and longitude are provided
-      if (latitude && longitude) {
-          // Set the device location on the map
-          setDeviceLocation(latitude, longitude);
-      } else {
-          // Show an alert if either latitude or longitude is missing
-          alert('Please enter both latitude and longitude.');
-      }
-  });
-  }
-  
-  // Add an event listener to initialize the map when the page loads
-  document.addEventListener('DOMContentLoaded', function() {
-    initMap();
-  });
+});
+}
 
-  // Function to remove all markers from the map
+// Add an event listener to initialize the map when the page loads
+document.addEventListener('DOMContentLoaded', function() {
+  initMap();
+});
+
+// Function to remove all markers from the map
 function resetMarkers() {
-  // Loop through all markers and remove them from the map
-  markers.forEach(function(marker) {
-      marker.setMap(null);
-  });
+// Loop through all markers and remove them from the map
+markers.forEach(function(marker) {
+    marker.setMap(null);
+});
 
-  // Clear the markers array
-  markers.length = 0;
+// Clear the markers array
+markers.length = 0;
 }
 
 // Add an event listener to the "Reset" button
 document.getElementById('resetButton').addEventListener('click', function() {
-  // Call the resetMarkers function to remove all markers
-  resetMarkers();
+// Call the resetMarkers function to remove all markers
+resetMarkers();
 });
-  
+
